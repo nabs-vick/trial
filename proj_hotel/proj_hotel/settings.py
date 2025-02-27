69c4069757b7c43cb1e6c3e9c9eef142f27ed259
vick <vickkemboi01@gmail.com>
2025-02-27 13:09:46 +0300

    first commit

--- proj_hotel/proj_hotel/settings.py
+++ proj_hotel/proj_hotel/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_i=x(=yxm!3lm&f6i%&ze!$pbenx&zj!f(*)!$q8x@x-qn#6(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['.vercel.app','127.0.0.1','.now.sh']
 
 
 # Application definition
